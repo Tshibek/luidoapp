dab03a292de19729ba718ecb9eb2aa9e86ddfd04
Kamil <tshibe95@gmail.com>
2021-08-29 12:25:17 +0200

    FIX FILTER

--- core/models.py
+++ core/models.py
@@ -33,8 +33,9 @@ class Monter(models.Model):
 
     def sum_daily_hours(self, month, year):
         sum_hours = \
-        list(MonterDaily.objects.filter(name__pk=self.pk, date__year=year, date__month=month,
-                                        ).filter(Q(status='PRACUJE') & Q(status='URLOP') & Q(status='NŻ')).aggregate(Sum('daily_hours')).values())[0]
+            list(MonterDaily.objects.filter(name__pk=self.pk, date__year=year, date__month=month,
+                                            status__in=['PRACUJE', 'URLOP', 'NŻ']
+                                            ).aggregate(Sum('daily_hours')).values())[0]
 
         a = sum_hours.total_seconds()
         h = a // 3600
@@ -54,7 +55,6 @@ class Monter(models.Model):
 
         context = locals()
         return context
-
 
 
 class Team(models.Model):
