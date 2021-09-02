49d514f1102e0710543a8c2923a5e60d0d3f3e11
Kamil <tshibe95@gmail.com>
2021-09-02 06:52:17 +0200

    check new lightbox

--- todo/urls.py
+++ todo/urls.py
@@ -6,5 +6,6 @@ from todo import views
 
 app_name = 'todo'
 urlpatterns = [
-    path('lista', views.todo_list, name='todo_list'),
+    path('/lista', views.todo_list, name='todo_list'),
+    path('/dodaj',views.todo_add, name='todo_add'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.utils import timezone
+
 from . import models
+from .forms import TodoForm
 
 
 # Create your views here.
@@ -12,11 +15,24 @@ def todo_list(request):
 
 
 def todo_add(request):
+    form_todo = TodoForm(request.POST)
+    if request.method == 'POST':
+        form_todo = TodoForm(request.POST)
+        if form_todo.is_valid():
+            instance = form_todo.save(commit=False)
+            instance.user = request.user
+            instance.date = timezone.now()
+            instance.save()
+            return redirect('todo:todo_list')
+        else:
+            form_todo = TodoForm(request.GET)
+
     context = locals()
     return render(request, 'todo/todo_add.html', context)
 
 
-def todo_update(request):
-    todos = models.Todo.objects.all()
+def todo_update(request, pk):
+    todos = models.Todo.objects.get(pk=pk)
+    # form_history_todo = TodoHistoryForm(request.POST)
     context = locals()
     return render(request, 'todo/todo.html', context)
