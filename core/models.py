43cac5a95882532c7fb7c4b0757b6485e215891f
Kamil <tshibe95@gmail.com>
2021-09-03 21:15:45 +0200

    Add scrap monthly hours

--- core/models.py
+++ core/models.py
@@ -47,11 +47,11 @@ class Monter(models.Model):
         date = datetime.now()
         bussines_day = len([x for x in cal.itermonthdays2(year, month) if x[0] != 0 and x[1] < 5])
         working_hours = bussines_day * 8
-        if h <= scrap_monthly_hours():
+        if h <= scrap_monthly_hours(month,year):
             sum_daily = "{}h {}m".format(int(h), int(m))
-        elif h > scrap_monthly_hours():
-            h = h - scrap_monthly_hours()
-            sum_daily = "{}h".format(scrap_monthly_hours())
+        elif h > scrap_monthly_hours(month,year):
+            h = h - scrap_monthly_hours(month,year)
+            sum_daily = "{}h".format(scrap_monthly_hours(month,year))
             under_daily = "{}h {}m".format(int(h), int(m))
 
         context = locals()
--- core/scrap_year_hours.py
+++ core/scrap_year_hours.py
@@ -2,13 +2,11 @@ import requests
 from bs4 import BeautifulSoup
 import datetime
 
-date = datetime.datetime.now()
-year = date.year
-month = datetime.datetime.now().month
-url = 'https://kalendarz.livecity.pl/czas-pracy/{}'.format(year)
 
 
-def scrap_monthly_hours():
+
+def scrap_monthly_hours(month,year):
+    url = 'https://kalendarz.livecity.pl/czas-pracy/{}'.format(year)
     request = requests.get(url)
     bs = BeautifulSoup(request.content, features="html.parser")
     hours = bs.find_all(attrs={"data-title": "godz. pracujące"})
