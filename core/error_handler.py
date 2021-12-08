a4083fb0f60b17eca0fa6df79cdbd7e4f26ad8d9
Tshibe-Win <tshibe95@gmail.com>
2021-12-08 07:40:34 +0100

    check errors

--- core/error_handler.py
+++ core/error_handler.py
@@ -3,9 +3,14 @@ from django.template import RequestContext
 
 
 def handler404(request, exception):
-    template_name="errors/404.html"
-    return render(request, template_name, status_code = 404)
+    context = {}
+    response = render(request, 'errors/404.html', context=context)
+    response.status_code = 404
+    return response
+
 
 def handler500(request, exception):
-    template_name="errors/500.html"
-    return render(request, template_name, status_code = 500)
+    context = {}
+    response = render(request, 'errors/500.html', context=context)
+    response.status_code = 500
+    return response
