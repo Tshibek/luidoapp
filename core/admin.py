75c0e83a79a9bcd1fb6fc5c78f23b1aa2ff187bc
Tshibe-Win <tshibe95@gmail.com>
2022-01-07 17:34:41 +0100

    repair dailymontage admin search

--- core/admin.py
+++ core/admin.py
@@ -74,10 +74,10 @@ class DailyEndWorkFilter(SimpleListFilter):
         ]
 
     def queryset(self, request, queryset):
-        if self.value() is None:
-            return queryset.distinct().filter(end_time__isnull=True)
         if self.value():
             return queryset.distinct().filter(end_time__isnull=True)
+        # if self.value():
+        #     return queryset.distinct().filter(end_time__isnull=True)
 
 
 @admin.register(models.MonterDaily)
